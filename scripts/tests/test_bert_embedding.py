0dc1880b77e1104e90fd15c96308764e5e64c9d2
Gary Lai <gary.lai@emory.edu>
2019-04-01 10:57:18 -0400

    Update test_bert_embedding.py
    -  add dtype to arguments

--- scripts/tests/test_bert_embedding.py
+++ scripts/tests/test_bert_embedding.py
@@ -57,18 +57,18 @@ def test_bert_embedding_data_loader_works_with_uncased_data():
 @pytest.mark.remote_required
 @pytest.mark.gpu
 def test_bert_embedding():
-    process = subprocess.check_call(['python', './scripts/bert/embedding.py', '--gpu', '0',
+    # Test command line interface.
+    # Sentences are passed through arguments.
+    process = subprocess.check_call(['python', './scripts/bert/embedding.py', '--gpu', '0', '--dtype', 'float32',
                                      '--model', 'bert_12_768_12', '--dataset_name', 'book_corpus_wiki_en_uncased',
                                      '--max_seq_length', '25', '--batch_size', '256',
                                      '--oov_way', 'avg', '--sentences', '"is this jacksonville ?"'])
     time.sleep(5)
-    
-    process = subprocess.check_call(['python', './scripts/bert/embedding.py', '--gpu', '0',
+    # Load params.
+    process = subprocess.check_call(['python', './scripts/bert/embedding.py', '--gpu', '0', '--dtype', 'float32',
                                      '--model', 'bert_12_768_12', '--dataset_name', 'book_corpus_wiki_en_uncased',
                                      '--max_seq_length', '25', '--batch_size', '256',
                                      '--params_path',
                                      '~/.mxnet/models/bert_12_768_12_book_corpus_wiki_en_uncased-75cc780f.params',
                                      '--oov_way', 'avg', '--sentences', '"is this jacksonville ?"', '--verbose'])
-
-
     time.sleep(5)
